7d91c78672e8274bee72e607427fb71b827dbb1c
sarojanakka <51170774+sarojanakka@users.noreply.github.com>
2021-06-13 20:34:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,72 @@
+import imghdr
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory
+from werkzeug.utils import secure_filename
+import humandetect 
+
+app = Flask(__name__, template_folder='templates')
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.jpeg', '.png', '.gif', '.mp4']
+app.config['UPLOAD_PATH'] = 'uploads\\'
+app.config['OUTPUT_PATH'] = 'outputs\\'
+
+def validate_image(stream):
+    header = stream.read(512)
+    stream.seek(0)
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
+
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
+
+@app.route('/')
+def index():
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template('index.html', files=files)
+
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+                file_ext != validate_image(uploaded_file.stream):
+            return "Invalid image", 400
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        humandetect.humanDetector(os.path.join(app.config['UPLOAD_PATH'], filename),None,None,os.path.join(app.config['OUTPUT_PATH'], filename))
+    return redirect(url_for('index'))
+    #return '', 204
+    
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+@app.route('/outputs/<filename>')
+def output(filename):
+    return send_from_directory(app.config['OUTPUT_PATH'], filename)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #<form action="{{ url_for('upload_files') }}" class="dropzone">
+    #</form>
+    #<hr>
+    #<div>
+      #<img src="{{ url_for('upload', filename=file) }}" style="width: 64px">
+    #</div>
+    
+    #<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.1/min/dropzone.min.js"></script>
